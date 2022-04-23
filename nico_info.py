faec03980de1a85063c9621aad60c67cab424c68
ogosk <ash_612@me.com>
2022-04-23 23:39:53 +0900

    initial commit

---
+++ nico_info.py
@@ -0,0 +1,421 @@
+import requests
+import json
+import datetime
+import pandas as pd
+
+from bs4 import BeautifulSoup
+from tqdm.auto import tqdm
+from typing import Union, List
+
+
+base_params = {
+    'version': '20090904',
+    'scores': '1',
+    'language': '0',
+    'force_184': '1',
+}
+
+# default: dict[key][0]
+basic_commands = {
+    'position': ['naka', 'ue', 'shita'],
+    'size': ['medium', 'small', 'big'],
+    'color': [
+        'white', 'red', 'pink', 'orange', 'yellow',
+        'green', 'cyan', 'blue', 'purple', 'black',
+        'white2', 'red2', 'pink2', 'orange2', 'yellow2',
+        'green2', 'cyan2', 'blue2', 'purple2', 'black2',
+        'niconicowhite', 'truered', 'passionorange', 'madyellow',
+        'elementalgreen', 'marineblue',  'nobleviolet'
+    ]
+}
+
+
+class NicovideoInfomation():
+    def __init__(self, video_url: str = None, video_id: str = None):
+        assert any([video_url, video_id])
+
+        if video_id:
+            video_url = f'https://www.nicovideo.jp/watch/{video_id}'
+
+        source = requests.get(video_url)
+
+        soup = BeautifulSoup(source.text, 'html.parser')
+        js = json.loads(
+            soup.select_one('#js-initial-watch-data').get('data-api-data')
+        )
+        threads = sorted(js['comment']['threads'], key=lambda x: x['fork'])
+
+        if len(threads) > 3:
+            threads.pop(0)
+
+        api_url = threads[0]['server'] + '/api.json'
+
+        post_time = datetime.datetime.strptime(
+            js['video']['registeredAt'], '%Y-%m-%dT%H:%M:%S+09:00'
+        ).timestamp()
+
+        video_id = js['video']['id']
+        video_title = js['video']['title']
+        video_counter = js['video']['count']
+        video_url = video_url[:video_url.index(video_id)+len(video_id)]
+
+        if js['owner']:
+            self.owner_id = js['owner']['id']
+            self.owner_name = js['owner']['nickname']
+        elif js['channel']:
+            self.owner_id = js['channel']['id']
+            self.owner_name = js['channel']['name']
+
+        self.video_id = video_id
+        self.video_title = video_title
+        self.video_url = video_url
+        self.video_counter = video_counter
+        self.post_time = post_time
+
+        self.api_url = api_url
+        self.threads = {d['fork']: d for d in threads}
+
+        self.comments_df = None
+
+    def load_comments(
+        self,
+        forks: Union[List, int] = [0, 1, 2],
+        mode: str = 'once',
+        hop_rate: float = .5,
+        check: bool = True,
+        **tqdm_kwargs
+    ):
+        assert type(forks) == int or all([type(fork) == int for fork in forks])
+        assert mode in ['once', 'roughly', 'exactly']
+
+        if type(forks) == int:
+            forks = [forks]
+
+        def fetch_comments(forks, when=None):
+            params_dict = {
+                fork: dict(
+                    **{
+                        'thread':    self.threads[fork]['id'],
+                        'fork':      self.threads[fork]['fork'],
+                        'threadkey': self.threads[fork]['threadkey'],
+                        'when': when
+                    },
+                    **base_params
+                )
+                for fork in forks
+            }
+
+            req = [
+                {k: params_dict[fork] for k in ['thread', 'thread_leaves']}
+                for fork in forks
+            ]
+
+            res = requests.get(self.api_url, data=json.dumps(req))
+            comments = [d['chat'] for d in json.loads(res.text) if 'chat' in d]
+
+            return comments
+
+        def convert_to_df(comments):
+            comments_dict = {}
+            for com in comments:
+                default_items = {
+                    'user_id':   None,
+                    'date_usec': 0,
+                    'anonymity': 0,
+                    'fork':      0,
+                    'score':     0,
+                    'mail':      None,
+                    'position':  basic_commands['position'][0],
+                    'size':      basic_commands['size'][0],
+                    'color':     basic_commands['color'][0]
+                }
+                for k, v in default_items.items():
+                    if k not in com:
+                        com[k] = v
+
+                if com['mail']:
+                    cmds = com['mail'].split()
+                    if '184' in cmds:
+                        if com['anonymity'] != 1:
+                            print('184 error.')
+                        _ = cmds.pop(cmds.index('184'))
+
+                    cmds_set = set(cmds)
+                    check_dict = {
+                        'position': cmds_set & set(basic_commands['position']),
+                        'size':     cmds_set & set(basic_commands['size']),
+                        'color':    cmds_set & set(basic_commands['color'])
+                    }
+
+                    for k, v in check_dict.items():
+                        if v:
+                            com[k] = cmds.pop(cmds.index(v.pop()))
+
+                    com['mail'] = ' '.join(cmds)
+
+                comment_id = f'{com["fork"]}-{com["no"]}'
+
+                vpos = '0' if com['vpos'] < 0 else str(com['vpos'])
+                write_time = float(f'{com["date"]}.{com["date_usec"]}')
+                video_time = float(f'{vpos[:-2]}.{vpos[-2:]}')
+
+                datas = {
+                    'comment':    com['content'],
+                    'user_id':    com['user_id'],
+                    'write_time': write_time,
+                    'video_time': video_time,
+                    '184':        com['anonymity'],
+                    'position':   com['position'],
+                    'size':       com['size'],
+                    'color':      com['color'],
+                    'command':    com['mail'],
+                    'score':      com['score']
+                }
+                datas = {k: v for k, v in datas.items() if v is not None}
+
+                comments_dict[comment_id] = datas
+
+            comments_df = pd.DataFrame.from_dict(comments_dict, orient='index')
+            if comments_dict:
+                comments_df.sort_values('write_time', inplace=True)
+                comments_df.index.name = 'comment_id'
+
+            return comments_df
+
+        def merge_df(a_df, b_df):
+            ab_df = pd.merge(
+                a_df.reset_index(), b_df.reset_index(), how='outer'
+            ).set_index('comment_id').sort_values('write_time')
+            return ab_df
+
+        def check_df(comments_df, tgt_forks):
+            got_forks = set(map(int, set(comments_df.index.str[0])))
+            tgt_forks = set(tgt_forks)
+            # 読み込みできなかった fork は最初から存在していないものとみなして消去
+            for fork in tgt_forks.copy():
+                if fork not in got_forks:
+                    tgt_forks.remove(fork)
+
+            # 全件読み込めている fork は消去
+            for fork in tgt_forks.copy():
+                fork_df = comments_df[comments_df.index.str[0] == str(fork)]
+                cids = sorted(list(map(int, fork_df.index.str[2:])))
+                if cids[-1] == len(cids):
+                    tgt_forks.remove(fork)
+
+            return sorted(list(tgt_forks))
+
+        now_time = datetime.datetime.now().timestamp()
+        now_df = convert_to_df(fetch_comments(forks))
+
+        comments_df = now_df
+        forks = check_df(comments_df, forks)
+
+        if mode == 'once' or not forks:
+            self.comments_df = comments_df
+            if check:
+                self.check_comments()
+
+            return comments_df
+
+        # 1時間前のコメントを基準にしてそこから遡って読み込む
+        tmp_time = now_time - 60*60
+        tmp_df = convert_to_df(fetch_comments(forks))
+
+        comments_df = merge_df(comments_df, tmp_df)
+        forks = check_df(comments_df, forks)
+
+        try_num = 1+1
+        with tqdm(total=int(now_time-self.post_time), **tqdm_kwargs) as pbar:
+            while forks:
+                try_num += 1
+                fork2tmp = {
+                    fork: tmp_df[tmp_df.index.str[0] == str(fork)]
+                    for fork in forks
+                }
+                tgt_time = max([
+                    fork2tmp[fork].write_time[int((len(fork2tmp[fork])-1)*hop_rate)]
+                    for fork in forks
+                ])
+                tgt_df = convert_to_df(
+                    fetch_comments(forks, when=tgt_time)
+                )
+                if tgt_df.empty:
+                    break
+
+                comments_df = merge_df(comments_df, tgt_df)
+                forks = check_df(comments_df, forks)
+
+                pbar.set_postfix(
+                    date=str(datetime.datetime.fromtimestamp(tgt_time).date()),
+                )
+                pbar.set_description(f'Loading roughly [{try_num}]')
+                pbar.update(int(tmp_time-tgt_time))
+
+                comp_list = [
+                    not (
+                        set(tgt_df[tgt_df.index.str[0] == str(fork)].index) - \
+                        set(tmp_df[tmp_df.index.str[0] == str(fork)].index)
+                    )
+                    for fork in forks
+                ]
+                if all(comp_list):
+                    break
+
+                tmp_time, tmp_df = tgt_time, tgt_df
+
+            pbar.update(pbar.total-pbar.n)
+
+        if mode == 'exactly':
+            fork2com = {
+                fork: comments_df[comments_df.index.str[0] == str(fork)]
+                for fork in forks
+            }
+            unload_cids = {
+                fork:
+                    sorted(list(
+                        set(range(1, len(fork2com[fork])+1)) - \
+                        set(map(int, fork2com[fork].index.str[2:]))
+                    ))
+                for fork in forks
+            }
+
+            avg_cnum = {
+                0: min(500, self.video_counter['comment']//10),
+                1: 1000,
+                2: 100
+            }
+            w_len = {k: int(v*.8) for k, v in avg_cnum.items()}
+
+            tgt_cids = {fork: [] for fork in forks}
+            for fork in forks:
+                r_cid = unload_cids[fork][-1]
+                l_cid, m_cid = r_cid-2*w_len[fork], r_cid-w_len[fork]
+
+                if m_cid < 0:
+                    tgt_cids[fork].append(r_cid)
+                    continue
+                else:
+                    tgt_cids[fork].append(m_cid)
+
+                for cid in unload_cids[fork][::-1]:
+                    if l_cid >= cid:
+                        r_cid = l_cid
+                        l_cid, m_cid = r_cid-2*w_len[fork], r_cid-w_len[fork]
+
+                        if r_cid < 0:
+                            break
+
+                        if m_cid < 0:
+                            tgt_cids[fork].append(r_cid)
+                            break
+                        else:
+                            tgt_cids[fork].append(m_cid)
+
+            tgt_whens = {}
+            for fork in forks:
+                cids = tgt_cids[fork]
+                com_cids = sorted(list(map(int, fork2com[fork].index.str[2:])))
+                tmp = []
+                for cid in cids:
+                    for ccid in com_cids:
+                        if ccid >= cid:
+                            tmp.append(f'{fork}-{ccid}')
+                            break
+
+                tgt_whens[fork] = fork2com[fork].loc[tmp, :].write_time.values
+
+            for fork in forks.copy():
+                for when in tqdm(
+                    tgt_whens[fork], desc=f'{fork}-Loading exactly', **tqdm_kwargs
+                ):
+                    tgt_df = convert_to_df(
+                        fetch_comments(forks, when=when)
+                    )
+                    comments_df = merge_df(comments_df, tgt_df)
+                    forks = check_df(comments_df, forks)
+
+                    if not forks:
+                        break
+
+        self.comments_df = comments_df
+
+        if check:
+            self.check_comments()
+
+    def check_comments(self):
+        if self.comments_df is not None:
+            total_df = self.comments_df
+            total_uids = set(total_df.user_id)
+            total_unum, total_cnum = len(total_uids), len(total_df)
+
+            forks = sorted(list(set([cid[0] for cid in total_df.index])))
+            forks_dfs = [total_df[total_df.index.str[0] == fork] for fork in forks]
+
+            print('=== total comments ===')
+            print('user number:', total_unum)
+            print('comment number:', total_cnum)
+            print(f'comment / user: {total_cnum/total_unum:.2f}')
+            print('------')
+            print(
+                'acquisition rate:',
+                f'{total_cnum/sum([int(df.index[-1][2:]) for df in forks_dfs]):.2%}'
+            )
+            if forks:
+                print()
+
+            for fork, fork_df in zip(forks, forks_dfs):
+                fork_uids = set(fork_df.user_id)
+                fork_cnum, fork_unum = len(fork_df), len(fork_uids)
+
+                print(f'=== comments: {fork} ===')
+                print('user number:', fork_unum)
+                print('comment number:', fork_cnum)
+                print(f'comment / user: {fork_cnum/fork_unum:.2f}')
+                print('------')
+                print(
+                    'acquisition rate:',
+                    f'{fork_cnum/int(fork_df.index[-1][2:]):.2%}'
+                )
+                if fork != forks[-1]:
+                    print()
+
+    def sort_comments(
+        self,
+        sort_items_list: List = ['write_time'],
+        ascending: bool = True
+    ):
+        if self.comments_df is not None:
+            tags, ascendings = [], []
+            for items in sort_items_list:
+                if type(items) == str:
+                    tags.append(items)
+                    ascendings.append(ascending)
+                elif type(items) == tuple:
+                    tags.append(items[0])
+                    if items[1] in [1, True]:
+                        ascendings.append(True)
+                    elif items[1] in [-1, False]:
+                        ascendings.append(False)
+                    else:
+                        ascendings.append(ascending)
+
+            sorted_df = self.comments_df.sort_values(
+                tags[::-1], ascending=ascendings[::-1]
+            )
+
+            return sorted_df
+
+    def video_html(self, w: int = 640, h: int = 360):
+        video_id = self.video_id
+        video_title = self.video_title
+        html = f'''
+            <script
+              type="application/javascript"
+              src="https://embed.nicovideo.jp/watch/{video_id}/script?w={w}&h={h}">
+            </script>
+            <noscript>
+                <a href="https://www.nicovideo.jp/watch/{video_id}">{video_title}</a>
+            </noscript>
+        '''
+        return html
---
+++ utils.py
@@ -0,0 +1,236 @@
+import yaml
+import numpy as np
+from kanjize import kanji2int
+
+
+def analyze_comments(
+    comments: list, pos: str = '名詞', tokenizer: str = 'janome'
+):
+    if tokenizer == 'janome':
+        from janome.tokenizer import Tokenizer
+        tokenizer = Tokenizer()
+
+        def tokenize(comment):
+            result = [
+                token.base_form
+                for token in tokenizer.tokenize(comment)
+                if token.part_of_speech.split(',')[0] in [pos]
+                and token.base_form not in exclude_noun
+                and not all([(s in n_chrs) for s in token.base_form])
+            ]
+            return [w for w in result if w]
+
+    elif tokenizer == 'sudachi':
+        from sudachipy import dictionary, tokenizer
+        tokenizer_obj = dictionary.Dictionary().create()
+
+        def tokenize(comment):
+            result = [
+                token.dictionary_form()
+                for token in tokenizer_obj.tokenize(
+                    comment, mode=tokenizer.Tokenizer.SplitMode.C
+                )
+                if token.part_of_speech()[0] in [pos]
+                and token.surface() not in exclude_noun
+                and not all([(s in n_chrs) for s in token.surface()])
+            ]
+            return [w for w in result if w]
+
+    # 絵文字や記号など必要ないものは(力技で)まとめて除去
+    symbol_s, symbol_e = 44, 57
+    Alphabet_s, Alphabet_e = 65, 90
+    alphabet_s, alphabet_e = 97, 122
+    Hiragana_s, Hiragana_e = 12353, 12438
+    Katakana_s, Katakana_e = 12449, 12534
+    katakana_s, katakana_e = 65382, 65439
+    Kanji_s, Kanji_e = 19968, 40959
+    exceptions = [
+        32,  # ' '
+        37,  # '%'
+        12289,  # '、'
+        12290,  # '。'
+        12540   # 'ー'
+    ]
+
+    zen2han = {chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}
+    etc_sym = {'〜': 'ー', '~': 'ー', '　': ' '}
+    replace_dict = str.maketrans(dict(zen2han, **etc_sym))
+
+    # 全角(アルファベット & 一部の記号) -> 半角 + 一部記号変換 + 不要な文字の大部分を除去
+    def preprocess(comment):
+        result = comment.translate(replace_dict)
+
+        def judge_mojicode(i):
+            flg = any([
+                (symbol_s <= i <= symbol_e),
+                (Alphabet_s <= i <= Alphabet_e or alphabet_s <= i <= alphabet_e),
+                (Hiragana_s <= i <= Hiragana_e),
+                (Katakana_s <= i <= Katakana_e or katakana_s <= i <= katakana_e),
+                (Kanji_s <= i <= Kanji_e),
+                (i in exceptions)
+            ])
+            return flg
+
+        result = ''.join([s for s in result if judge_mojicode(ord(s))])
+
+        return result
+
+    comments = [preprocess(comment) for comment in comments]
+    comments = [comment for comment in comments if len(comment) > 1]
+
+    if pos == '名詞':
+        with open('data/chr.yaml', mode='rb') as f:
+            chrs = yaml.load(f, Loader=yaml.SafeLoader)
+
+        with open('data/exclude_noun.yaml', mode='rb') as f:
+            exclude_noun = yaml.load(f, Loader=yaml.SafeLoader)
+
+        kanas = chrs['hiraganas']+chrs['katakanas']+chrs['hankanas']
+        alphabets = chrs['Alphabets']+chrs['alphabets']
+
+        units = exclude_noun.pop('units')
+        slang = exclude_noun.pop('slang')
+        n_chrs = exclude_noun.pop('n_chrs')
+
+        elems = [v for v in exclude_noun.values()] + [kanas, alphabets]
+        tmp = []
+        _ = [tmp.extend(elem) for elem in elems]
+        exclude_noun = tmp
+
+        results = []
+        for comment in comments:
+            if comment not in slang:
+                result = tokenize(comment)
+                results.extend(result)
+
+        tmp = []
+        while len(results) > 1:
+            if results[1] in units:
+                # n + 単位で表せるものは連結
+                if results[0].isdigit():
+                    k = results.pop(0)
+                elif kanji2int(results[0]) > 0:
+                    k = str(kanji2int(results.pop(0)))
+                else:
+                    k = False
+
+                # さらに n 回目などと表せるものは連結
+                if k and len(results) > 1 and results[1] in [
+                    '目', '分', '前', '後', '以内', '以上', '以下' '未満', '強', '弱'
+                ]:
+                    tmp.append(k+results.pop(0)+results.pop(0))
+                elif k:
+                    tmp.append(k+results.pop(0))
+                else:
+                    tmp.append(results.pop(0))
+
+            else:
+                tmp.append(results.pop(0))
+
+        if results:
+            tmp.append(results.pop(0))
+
+        results = tmp
+
+        # 1 や 三 など 1 文字の数字は除去
+        results = [
+            result for result in results
+            if not len(result) == 1
+            or (not result.isdigit() and kanji2int(result) == 0)
+        ]
+
+    return results
+
+
+def comments2vec(comments: list, model, tokenizer: str = 'janome'):
+    exclude_pos0 = [] #['助詞', '助動詞', '接頭詞', '接続詞', '連体詞', '記号', 'フィラー']
+    exclude_pos1 = [] #['代名詞', '接尾', '非自立', '数']
+    if tokenizer == 'janome':
+        from janome.tokenizer import Tokenizer
+        tokenizer = Tokenizer()
+
+        def tokenize(comment):
+            result = [
+                # token.base_form
+                token.surface
+                for token in tokenizer.tokenize(comment)
+                if token.part_of_speech.split(',')[0] not in exclude_pos0
+                and token.part_of_speech.split(',')[1] not in exclude_pos1
+                #if not all([(s in n_chrs) for s in token.base_form])
+            ]
+            return [w for w in result if w]
+
+    elif tokenizer == 'sudachi':
+        from sudachipy import dictionary, tokenizer
+        tokenizer_obj = dictionary.Dictionary().create()
+
+        def tokenize(comment):
+            result = [
+                # token.dictionary_form()
+                token.surface()
+                for token in tokenizer_obj.tokenize(
+                    comment, mode=tokenizer.Tokenizer.SplitMode.C
+                )
+                if token.part_of_speech()[0] in exclude_pos0
+                and token.part_of_speech()[1] not in exclude_pos1
+                #if not all([(s in n_chrs) for s in token.surface()])
+            ]
+            return [w for w in result if w]
+   
+    symbol_s, symbol_e = 44, 57
+    Alphabet_s, Alphabet_e = 65, 90
+    alphabet_s, alphabet_e = 97, 122
+    Hiragana_s, Hiragana_e = 12353, 12438
+    Katakana_s, Katakana_e = 12449, 12534
+    katakana_s, katakana_e = 65382, 65439
+    Kanji_s, Kanji_e = 19968, 40959
+    exceptions = [
+        32,  # ' '
+        37,  # '%'
+        12289,  # '、'
+        12290,  # '。'
+        12540   # 'ー'
+    ]
+
+    zen2han = {chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}
+    etc_sym = {'〜': 'ー', '~': 'ー', '　': ' '}
+    replace_dict = str.maketrans(dict(zen2han, **etc_sym))
+
+    # 全角(アルファベット & 一部の記号) -> 半角 + 一部記号変換 + 不要な文字の大部分を除去
+    def preprocess(comment):
+        result = comment.translate(replace_dict)
+
+        def judge_mojicode(i):
+            flg = any([
+                (symbol_s <= i <= symbol_e),
+                (Alphabet_s <= i <= Alphabet_e or alphabet_s <= i <= alphabet_e),
+                (Hiragana_s <= i <= Hiragana_e),
+                (Katakana_s <= i <= Katakana_e or katakana_s <= i <= katakana_e),
+                (Kanji_s <= i <= Kanji_e),
+                (i in exceptions)
+            ])
+            return flg
+
+        result = ''.join([s for s in result if judge_mojicode(ord(s))])
+
+        return result if len(result) >= 10 else None
+
+    def text2vec(comment, model):
+        vec = model.infer_vector([
+            token
+            for token in tokenize(preprocess(comment))
+            if token
+        ])
+        return vec
+
+    comments = [preprocess(comment) for comment in comments]
+    comments = [comment for comment in comments if comment]
+    text = ' '.join(comments)
+
+    if text:
+        vec = text2vec(text, model)
+        w = len(comments)
+
+        return w, vec
+    else:
+        return None, None
